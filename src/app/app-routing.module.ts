f857dbb82ea39b8e744e49b6c4be9967e073fd7f
Jormillo <jonaybrito15@gmail.com>
2021-05-27 20:25:07 +0200

    Terminado el diseño de la interfaz dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [{
+const routes: Routes = [
+  {
   path : "",
-  component : AppComponent
+  component : HomeComponent
 },
 {
   path : "dashboard",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
+// import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DashboardComponent
+    //ProfileComponent
   ],
   imports: [
     BrowserModule,
