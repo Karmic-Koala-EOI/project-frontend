11d7089937deab22e36bcdbf49119901e18d300d
irenegv22 <irene.gv94@gmail.com>
2021-05-28 16:08:38 +0100

    remind-password component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,10 +17,10 @@ const routes: Routes = [{
   path: 'register',
   component: RegisterComponent,
 },
-// {
-//   path: 'remind-password',
-//   component: RemindPasswordComponent,
-// }
+ {
+   path: 'remind-password',
+   component: RemindPasswordComponent,
+ }
 
 ];
 
---
+++ src/app/components/remind-password/remind-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remind-password',
+  templateUrl: './remind-password.component.html',
+  styleUrls: ['./remind-password.component.css']
+})
+export class RemindPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
