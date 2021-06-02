a224a8b2a37229fe539533fb35671691fadb20da
irenegv22 <irene.gv94@gmail.com>
2021-06-02 13:39:13 +0100

    legal Terms info empezado

--- src/app/components/rpg-legal-terms/rpg-legal-terms.component.ts
+++ src/app/components/rpg-legal-terms/rpg-legal-terms.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-rpg-legal-terms',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rpg-legal-terms.component.css']
 })
 export class RpgLegalTermsComponent implements OnInit {
+  user: any = null;
 
-  constructor() { }
+  constructor(private AuthService : AuthService) { }
 
   ngOnInit(): void {
+    this.getUserLogged();
+  }
+
+  getUserLogged() {
+    this.AuthService.getUserLogged()
+      .then(user => user ? this.user = user : this.user = null)
+  }
+
+  logout() {
+    this.AuthService.logout();
   }
 
 }
