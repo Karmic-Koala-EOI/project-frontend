f27afa02358ce73d7fd010abb46a94ad850e3910
santiagobrito94 <ssanti2006@hotmail.com>
2021-06-11 19:31:31 +0100

    Set id cookie in frontend

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,13 +10,18 @@ import { AuthService } from '../../services/auth.service';
 export class HomeComponent implements OnInit {
 
   tokenGoogle : string = "";
+  idUsuario : string = "";
 
   constructor(private route : ActivatedRoute, private AuthService :  AuthService) {
     this.route.queryParams.subscribe(params => {
       this.tokenGoogle = params['google'];
+      this.idUsuario = params['id'];
     });
     if(this.tokenGoogle !== undefined) {
       this.AuthService.setToken(this.tokenGoogle);
+    }
+    if(this.idUsuario !== undefined) {
+      this.AuthService.setUser(this.idUsuario);
     }
   }
 
